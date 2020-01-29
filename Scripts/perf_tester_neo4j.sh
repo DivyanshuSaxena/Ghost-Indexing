386b467d752504f065a38a502f5e969a0f24bcef
DivyanshuSaxena <dssaxena2011@gmail.com>
2020-01-30 05:29:46 +0530

    Change in format of perf_tester_neo4j.sh

--- Scripts/perf_tester_neo4j.sh
+++ Scripts/perf_tester_neo4j.sh
@@ -6,19 +6,19 @@
 # $2 : Label of vertex on which index is to be made
 # $3 : Attribute on which index is to be made
 # $4 : Whether the index is (date (D)/string (S)/integer (I)) type
-# $5: Column in csv on which index is to be made
+# $5 : Column in csv on which index is to be made (Count starting from 1)
 # $6 : Index Type (B/BP)
 # $7 : Branch size of B/BP Tree 
-# $8 : Type of data node
-# $9 : Fullpath to Neo4J Installation directory
-# $10 : Fullpath to ldbc_snb_datagen folder [Note: All datasets must be mentioned as social_network_neo_<number>]
-# $11 : Fullpath to ldbc_snb_implementations folder
-# $12 : Fullpath to Ghost-Indexing/Neo4j folder
-# $13 : Fullpath to the input csv
+# $8 : Parameter for which date conversion is to be done (-1 if no date param conversion needed)
+# $9 : Name of the input csv
+# $10 : Fullpath to Neo4J Installation directory
+# $11 : Fullpath to ldbc_snb_datagen folder [Note: All datasets must be mentioned as social_network_neo_<number>]
+# $12 : Fullpath to ldbc_snb_implementations folder
+# $13 : Fullpath to Ghost-Indexing/Neo4j folder
 
 datasets=( 1000 2500 4000 7500 )
 cwd=$(pwd)
-export NEO4J_HOME=$9
+export NEO4J_HOME=${10}
 export NEO4J_DB_DIR=$NEO4J_HOME/data/databases/graph.ghostdb  
 export POSTFIX=_0_0.csv
 
@@ -27,22 +27,23 @@ for dataset in ${datasets[*]}; do
 	echo "Starting for dataset: "$dataset
 	echo "--------------------------------------------------"
 
-  export NEO4J_DATA_DIR=${10}/social_network_neo_$dataset/
-  cd ${11}/cypher/load-scripts
+  export NEO4J_DATA_DIR=${11}/social_network_neo_$dataset/
+  cd ${12}/cypher/load-scripts
   # Deletes previous data in Neo4J, rewrites the new dataset and start Neo4j
   ./load-in-one-step.sh
 
   # Run the Neo4J queries over Lucene Indexes
-  cd ${12}/queries
-  ./run_all_params.sh $1 $5 $dataset 0 $9/bin/cypher-shell ${12}/../JanusGraph/GhostIndex ${11}/cypher/queries neo4j ghost2019
+  cd ${13}/queries
+  ./run_all_params.sh $1 $8 $dataset 0 ${10}/bin/cypher-shell ${13}/../JanusGraph/GhostIndex ${12}/cypher/queries neo4j ghost2019
 
   # Generate the ghost indexes
-  cd ${12}/IndexHandler
-  time ./unifyIndex.sh $2 $3_index_$6_$7 $7 $5 ${13} $4 $6 $3 $8 $9 neo4j ghost2019
+  cd ${13}/IndexHandler
+  echo "time ./unifyIndex.sh $2 $3_index_$6_$7 $7 $5 ${11}/social_network_neo_$dataset/$9 $4 $6 $3 ${10} neo4j ghost2019"
+  time ./unifyIndex.sh $2 $3_index_$6_$7 $7 $5 ${11}/social_network_neo_$dataset/$9 $4 $6 $3 ${10} neo4j ghost2019
 
   # Run the Neo4J queries over custom-made ghost indexes
-  cd ${12}/queries
-  ./run_all_params.sh $1 $5 $dataset 1 $9/bin/cypher-shell ${12}/../JanusGraph/GhostIndex ${12}/ghost-queries neo4j ghost2019
+  cd ${13}/queries
+  ./run_all_params.sh $1 $8 $dataset 1 ${10}/bin/cypher-shell ${13}/../JanusGraph/GhostIndex ${13}/ghost-queries neo4j ghost2019
 done
 
 echo "Done"
