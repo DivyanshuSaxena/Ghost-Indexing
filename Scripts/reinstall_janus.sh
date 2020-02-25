ffb7ae0ab592fb9374cb856e9b7f23b0369583a8
Divyanshu Saxena <dssaxena2011@gmail.com>
2020-02-25 14:53:28 +0530

    Re-install janus baadal configuration

--- Scripts/reinstall_janus.sh
+++ Scripts/reinstall_janus.sh
@@ -1,7 +1,9 @@
 rm -rf ~/Installers/janusgraph
 cd ~/Installers/
+cp tarballs/janusgraph-0.4.0-hadoop2.zip .
 unzip janusgraph-0.4.0-hadoop2.zip
+rm janusgraph-0.4.0-hadoop2.zip
 mv janusgraph-0.4.0-hadoop2 janusgraph
-cp -r janusgraph_0.2.1/ext/commons-csv janusgraph/ext/
+cp -r janusgraph_0.2.2/ext/commons-csv janusgraph/ext/
 rm janusgraph/lib/org.apache.servicemix.bundles.commons-csv-1.0-r706900_3.jar
 
