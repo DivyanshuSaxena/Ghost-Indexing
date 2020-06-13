75d8e172a3faabc5dbf73f920c7c33134fc64848
DivyanshuSaxena <dssaxena2011@gmail.com>
2020-06-13 15:21:55 +0530

    Reorganized loader and indexing scripts and conf files in GhostIndex

--- JanusGraph/IndexHandler/unifyIndex.sh
+++ JanusGraph/IndexHandler/unifyIndex.sh
@@ -21,51 +21,46 @@ echo "[INFO]: Generating Index files";
 echo "[INFO]: csv files generated"
 #exit 0
 
-CONFIG="graph = JanusGraphFactory.open('conf\/janusgraph-cassandra-es.properties')"
+CONFIG="graph = JanusGraphFactory.open('${PWD}/../GhostIndex/conf/janusgraph-cassandra-es.properties');"
 if [[ ${10} -gt 0 ]]; then
     CONFIG="graph = JanusGraphFactory.build().set('storage.backend', 'cassandrathrift').set('storage.hostname', '10.17.5.53').set('index.search.backend', 'elasticsearch').set('index.search.hostname', '10.17.5.53:9210').open();"
 fi
 
-PWD_ORIG=$(pwd)
+PWD_STR=$(pwd)
 TYPE="string"
 if [ "$6" = "D" ]; then
     TYPE="date"
 fi
-echo "__$PWD_ORIG"
-PWD_ORIG_STR="${PWD_ORIG//\//\\\/}"
-echo "--$PWD_ORIG_STR"
 
-sed -i 's/indexVertices.csv/'"${PWD_ORIG_STR}"'\/indexVertices.csv/' indexCreationScripts/read_frm_file_${TYPE}.gremlin
-sed -i 's/indexEdges.csv/'"${PWD_ORIG_STR}"'\/indexEdges.csv/' indexCreationScripts/read_frm_file_${TYPE}.gremlin
-sed -i 's/indexDataEdges.csv/'"${PWD_ORIG_STR}"'\/indexDataEdges.csv/' indexCreationScripts/read_frm_file_${TYPE}.gremlin
-#echo "yo man ${TYPE} 0: $0 8: $8"
-sed -i "s/'id'/'$8'/" indexCreationScripts/read_frm_file_${TYPE}.gremlin
-sed -i "s/'@ATTRIBUTE'/'$9'/" indexCreationScripts/read_frm_file_${TYPE}.gremlin
-sed -i "s/'@INDEX_TYPE'/'$7'/" indexCreationScripts/read_frm_file_${TYPE}.gremlin
-sed -i "s/@CONFIG/${CONFIG}/" indexCreationScripts/read_frm_file_${TYPE}.gremlin
-echo "bye man";
-sed -i 's/leafEdges.csv/'"${PWD_ORIG_STR}"'\/leafEdges.csv/' indexCreationScripts/read_frm_file_leaves.gremlin
-sed -i "s/@CONFIG/${CONFIG}/" indexCreationScripts/read_frm_file_leaves.gremlin
+sed -i "s|indexVertices.csv|${PWD_STR}/indexVertices.csv|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|indexEdges.csv|${PWD_STR}/indexEdges.csv|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|indexDataEdges.csv|${PWD_STR}/indexDataEdges.csv|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|\"id\"|\"$8\"|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|@ATTRIBUTE|$9|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|@INDEX_TYPE|$7|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|@CONFIG|${CONFIG}|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|leafEdges.csv|${PWD_STR}/leafEdges.csv|" indexCreationScripts/read_from_file_leaves.gremlin
+sed -i "s|@CONFIG|${CONFIG}|" indexCreationScripts/read_from_file_leaves.gremlin
 
 cd $JANUS_HOME
-echo ":load ${PWD_ORIG}/indexCreationScripts/read_frm_file_${TYPE}.gremlin" | bin/gremlin.sh
+echo ":load ${PWD_STR}/indexCreationScripts/read_from_file_${TYPE}.gremlin" | bin/gremlin.sh
 
 if [ "$7" = "BP" ]; then
-    echo ":load ${PWD_ORIG}/indexCreationScripts/read_frm_file_leaves.gremlin" | bin/gremlin.sh
+    echo ":load ${PWD_STR}/indexCreationScripts/read_from_file_leaves.gremlin" | bin/gremlin.sh
 fi
 
 
 ### CLEAN UP
-cd $PWD_ORIG
-sed -i 's/'"${PWD_ORIG_STR}"'\/indexVertices.csv/indexVertices.csv/' indexCreationScripts/read_frm_file_${TYPE}.gremlin
-sed -i 's/'"${PWD_ORIG_STR}"'\/indexEdges.csv/indexEdges.csv/' indexCreationScripts/read_frm_file_${TYPE}.gremlin
-sed -i 's/'"${PWD_ORIG_STR}"'\/indexDataEdges.csv/indexDataEdges.csv/' indexCreationScripts/read_frm_file_${TYPE}.gremlin
-sed -i "s/'$8'/'id'/" indexCreationScripts/read_frm_file_${TYPE}.gremlin
-sed -i "s/'$9'/'@ATTRIBUTE'/" indexCreationScripts/read_frm_file_${TYPE}.gremlin
-sed -i "s/'$7'/'@INDEX_TYPE'/" indexCreationScripts/read_frm_file_${TYPE}.gremlin
-sed -i "s/${CONFIG}/@CONFIG/" indexCreationScripts/read_frm_file_${TYPE}.gremlin
-sed -i 's/'"${PWD_ORIG_STR}"'\/leafEdges.csv/leafEdges.csv/' indexCreationScripts/read_frm_file_leaves.gremlin
-sed -i "s/${CONFIG}/@CONFIG/" indexCreationScripts/read_frm_file_leaves.gremlin
+cd $PWD_STR
+sed -i "s|${PWD_STR}/indexVertices.csv|indexVertices.csv|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|${PWD_STR}/indexEdges.csv|indexEdges.csv|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|${PWD_STR}/indexDataEdges.csv|indexDataEdges.csv|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|\"$8\"|\"id\"|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|$9|@ATTRIBUTE|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|$7|@INDEX_TYPE|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|${CONFIG}|@CONFIG|" indexCreationScripts/read_from_file_${TYPE}.gremlin
+sed -i "s|${PWD_STR}/leafEdges.csv|leafEdges.csv|" indexCreationScripts/read_from_file_leaves.gremlin
+sed -i "s|${CONFIG}|@CONFIG|" indexCreationScripts/read_from_file_leaves.gremlin
 
 echo "Cleanup done"
 
